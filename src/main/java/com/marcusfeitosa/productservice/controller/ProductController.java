80f8ab640f15c52727f91a71cb88a8d1b8e3009e
marcus_feitosa <marcusfeitosa02@gmail.com>
2023-10-27 11:50:23 -0300

    Product - Post and Get methods

--- src/main/java/com/marcusfeitosa/productservice/controller/ProductController.java
+++ src/main/java/com/marcusfeitosa/productservice/controller/ProductController.java
@@ -1,11 +1,14 @@
 package com.marcusfeitosa.productservice.controller;
 
 import com.marcusfeitosa.productservice.dto.ProductDTO;
+import com.marcusfeitosa.productservice.dto.ProductResponse;
 import com.marcusfeitosa.productservice.model.Product;
 import com.marcusfeitosa.productservice.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
@@ -17,5 +20,11 @@ public class ProductController {
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductDTO productDTO){
         productService.createProduct(productDTO);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts(){
+        return productService.getAllProducts();
     }
 }
--- src/main/java/com/marcusfeitosa/productservice/service/ProductService.java
+++ src/main/java/com/marcusfeitosa/productservice/service/ProductService.java
@@ -1,11 +1,14 @@
 package com.marcusfeitosa.productservice.service;
 
 import com.marcusfeitosa.productservice.dto.ProductDTO;
+import com.marcusfeitosa.productservice.dto.ProductResponse;
 import com.marcusfeitosa.productservice.model.Product;
 import com.marcusfeitosa.productservice.repository.ProductRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -22,5 +25,18 @@ public class ProductService {
 
         productRepository.save(product);
         log.info("Product {} is saved", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts(){
+      List<Product> products = productRepository.findAll();
+      return products.stream().map(this::mapToProductResponde).toList();
+    }
+    private ProductResponse mapToProductResponde(Product product){
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
     }
 }
